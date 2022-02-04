4f8cc885b24a5bf42400634e912ece36410d4402
unknown <omkar.mishra_ug22@ashoka.edu.in>
2022-02-04 19:44:36 +0530

    readme

--- sawo/README.md
+++ sawo/README.md
@@ -1,70 +1,60 @@
-# Getting Started with Create React App
+## Table of contents
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
 
-## Available Scripts
+**Note: Delete this note and update the table of contents based on what sections you keep.**
 
-In the project directory, you can run:
+## Overview
 
-### `npm start`
+### The challenge
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+Users should be able to:
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+- Login using SAWO sdk (the passwordless way)
+- View the optimal layout for each page depending on their device's screen size
+- See their own IP address on the map on the initial page load
+- Search for any IP addresses or domains and see the key information and location
 
-### `npm test`
+### Screenshot
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+![](Screenshot 2022-02-04 192700.png)
+![](Screenshot 2022-02-04 192731.png)
+![](Screenshot 2022-02-04 192801.png)
 
-### `npm run build`
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+### Links
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+- Solution URL: https://github.com/omi265/sawo
+- Live Site URL: https://iptracker265.herokuapp.com/
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+## My process
 
-### `npm run eject`
+- Creating a react-leaflet [React-Leaflet](https://react-leaflet.js.org/) Map environment to map coordinates
+- integrating a searchbox to take user input
+- Fetching details according to the user input from the  [IP Geolocation API by IPify](https://geo.ipify.org/) API.
+- Using the response json to populate the values for mapping and description.
+- Designing the Landing page using CSS.
+- implementing the SAWO sdk Login setup. 
 
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
+### Built with
 
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- [React](https://reactjs.org/) - JS library
+- [Next.js](https://nextjs.org/) - React framework
+- [IP Geolocation API by IPify](https://geo.ipify.org/)
+- [LeafletJS](https://leafletjs.com/)
+- [React-Leaflet](https://react-leaflet.js.org/)
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+### Bugs Found
+These were a few bugs i found on the Sawo website while i was going through it.
+- ![](Screenshot 2022-02-04 193821.png)
+- ![](WhatsApp Image 2022-02-04 at 6.21.06 PM.jpeg)
--- sawo/src/App.js
+++ sawo/src/App.js
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from "react";
 import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import "./App.css";
 import L from "leaflet";
+import Sawo from "sawo";
 
-const api_key = "at_uSCpMmWvjI0jO8GWetMeSe5HtBwHI"
+const api_key = "at_uSCpMmWvjI0jO8GWetMeSe5HtBwHI";
 function GetIcon() {
   return L.icon({
     iconUrl: require("./icon-location.png"),
@@ -22,11 +23,14 @@ function App() {
   const [isp, setIsp] = useState("");
   const [ip, setIp] = useState();
   const [error, setError] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   function def_loc(inp) {
-    setError('')
+    setError("");
     fetch(
-      "https://geo.ipify.org/api/v2/country,city?apiKey=" + api_key + "&ipAddress=" +
+      "https://geo.ipify.org/api/v2/country,city?apiKey=" +
+        api_key +
+        "&ipAddress=" +
         inp
     )
       .then((res) => res.json())
@@ -56,6 +60,20 @@ function App() {
   }
 
   useEffect(() => {
+    const config = {
+      // should be same as the id of the container created on 3rd step
+      containerID: "sawo-container",
+      // can be one of 'email' or 'phone_number_sms'
+      identifierType: "phone_number_sms",
+      // Add the API key copied from 2nd step
+      apiKey: "727d5a5e-8938-44a1-9a4f-86f0c7db4d4e",
+      // Add a callback here to handle the payload sent by sdk
+      onSuccess: (payload) => {
+        setIsLoggedIn(true);
+      },
+    };
+    let sawo = new Sawo(config);
+    sawo.showForm();
     def_loc("");
   }, []);
 
@@ -63,62 +81,83 @@ function App() {
 
   return (
     <div className="App">
-      <div className="banner">
-        <h1 class="title">IP Address Tracker</h1>
-        <div className="searchbox">
-          <input
-            className="input_box"
-            type="text"
-            placeholder="Search for any IP address or domain"
-            onChange={(e) => {
-              setInput(e.target.value);
-            }}
-          ></input>
-          <button className="search_arrow" onClick={handleClick}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="11" height="14">
-              <path
-                fill="none"
-                stroke="#FFF"
-                stroke-width="3"
-                d="M2 1l6 6-6 6"
-              />
-            </svg>
-          </button>
-          {error ? <h4 className="alert">{error}</h4> : <h4></h4>}
-        </div>
+      
+      {/* // Login not working, therefore only container exists and the application is still running */}
+
+      {/* {isLoggedIn ? <h4 className="alert">LOGGED IN</h4> 
+      : <div>
+        <div
+          id="sawo-container"
+          style={{ height: "300px", width: "400px" }}
+        ></div>
       </div>
-      <div className="desc_box">
-        <div class="description">
-          <h4 class="desc_title">IP Address</h4>
-          <span>{ip}</span>
-        </div>
-        <div class="description">
-          <h4 class="desc_title">Location</h4>
-          <span>
-            {city}, {region}, {postal}
-          </span>
-        </div>
-        <div class="description">
-          <h4 class="desc_title">Timezone</h4>
-          <span>UTC {time}</span>
-        </div>
-        <div class="description description_last">
-          <h4 class="desc_title">ISP</h4>
-          <span>{isp}</span>
-        </div>
+      } */}
+
+      <div>
+        <div
+          id="sawo-container"
+          style={{ height: "300px", width: "400px" }}
+        ></div>
       </div>
-      <MapContainer
-        key={JSON.stringify([latitude, longitude])}
-        center={[latitude, longitude]}
-        zoom={13}
-        scrollWheelZoom={true}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[latitude, longitude]} icon={GetIcon()}></Marker>
-      </MapContainer>
+
+      <div>
+        <div className="banner">
+          <h1 class="title">IP Address Tracker</h1>
+          <div className="searchbox">
+            <input
+              className="input_box"
+              type="text"
+              placeholder="Search for any IP address or domain"
+              onChange={(e) => {
+                setInput(e.target.value);
+              }}
+            ></input>
+            <button className="search_arrow" onClick={handleClick}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="11" height="14">
+                <path
+                  fill="none"
+                  stroke="#FFF"
+                  stroke-width="3"
+                  d="M2 1l6 6-6 6"
+                />
+              </svg>
+            </button>
+            {error ? <h4 className="alert">{error}</h4> : <h4></h4>}
+          </div>
+        </div>
+        <div className="desc_box">
+          <div class="description">
+            <h4 class="desc_title">IP Address</h4>
+            <span>{ip}</span>
+          </div>
+          <div class="description">
+            <h4 class="desc_title">Location</h4>
+            <span>
+              {city}, {region}, {postal}
+            </span>
+          </div>
+          <div class="description">
+            <h4 class="desc_title">Timezone</h4>
+            <span>UTC {time}</span>
+          </div>
+          <div class="description description_last">
+            <h4 class="desc_title">ISP</h4>
+            <span>{isp}</span>
+          </div>
+        </div>
+        <MapContainer
+          key={JSON.stringify([latitude, longitude])}
+          center={[latitude, longitude]}
+          zoom={13}
+          scrollWheelZoom={true}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[latitude, longitude]} icon={GetIcon()}></Marker>
+        </MapContainer>
+      </div>
     </div>
   );
 }
